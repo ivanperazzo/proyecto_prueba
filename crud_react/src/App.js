fb0ec25a66529a18d43f412a5b067ae525301b83
Ivan Perazzo <iperazzo@hotmail.com.ar>
2021-12-30 20:47:27 -0300

    se agrega modal editar y eliminar.

--- crud_react/src/App.js
+++ crud_react/src/App.js
@@ -13,6 +13,10 @@ function App() {
 
   const [modalInsertar, setModalInsertar] = useState(false);
 
+  const [modalEditar, setModalEditar] = useState(false);
+
+  const [modalEliminar, setModalEliminar] = useState(false);
+
   const [gestorSeleccionado, setGestorSeleccionado] = useState({
     id: '', 
     nombre: '', 
@@ -31,6 +35,12 @@ function App() {
 
   const abrirCerrarModalInsertar=()=>{ 
     setModalInsertar(!modalInsertar);
+  }
+  const abrirCerrarModalEditar=()=>{ 
+    setModalEditar(!modalEditar);
+  }
+  const abrirCerrarModalEliminar=()=>{ 
+    setModalEliminar(!modalEliminar);
   }
 
   const peticionGet=async() =>{
@@ -52,6 +62,43 @@ function App() {
     }).catch(error =>{
       console.log(error);
     })
+  }
+
+  const peticionPut=async() =>{
+    gestorSeleccionado.lanzamiento = parseInt(gestorSeleccionado.lanzamiento);
+    await axios.put(baseUrl+"/"+gestorSeleccionado.id, gestorSeleccionado)
+    .then(response =>{
+      var respuesta=response.data;
+      var dataAuxiliar=data;      
+      dataAuxiliar.map(gestor=>{
+        if(gestor.id===gestorSeleccionado.id)
+        {
+          gestor.nombre=respuesta.nombre;
+          gestor.lanzamiento=respuesta.lanzamiento;
+          gestor.desarrollador=respuesta.desarrollador;
+        }
+      })
+      abrirCerrarModalEditar();
+    }).catch(error =>{
+      console.log(error);
+    })
+  }
+
+  const peticionDelete=async() =>{
+    await axios.delete(baseUrl+"/"+gestorSeleccionado.id)
+    .then(response =>{
+      setData(data.filter(gestor=>gestor.id!==response.data))
+      abrirCerrarModalEliminar();
+      }
+    ).catch(error =>{
+        console.log(error);
+      })
+  }
+
+  const seleccionarGestor=(gestor, caso) => {
+    setGestorSeleccionado(gestor);
+    (caso=="Editar")?
+    abrirCerrarModalEditar():abrirCerrarModalEliminar();
   }
 
   useEffect(()=>{
@@ -79,8 +126,8 @@ function App() {
               <td>{gestor.lanzamiento}</td>
               <td>{gestor.desarrollador}</td>
               <td>
-                <button className="btn btn-primary">Editar</button> {" "}
-                <button className="btn btn-danger">Eliminar</button> {" "}
+                <button className="btn btn-primary" onClick={()=>seleccionarGestor(gestor, "Editar")}>Editar</button> {" "}
+                <button className="btn btn-danger" onClick={()=>seleccionarGestor(gestor, "Eliminar")}>Eliminar</button> {" "}
               </td>
             </tr>))}
         </tbody>
@@ -109,6 +156,44 @@ function App() {
           <button className="btn btn-danger" onClick={()=>abrirCerrarModalInsertar()}>Cancelar</button>
         </ModalFooter>
       </Modal>
+
+<Modal isOpen={modalEditar}>
+  <ModalHeader>Editar Gestor de Base de Datos</ModalHeader>
+  <ModalBody>
+    <div className="form-group">
+      <label>ID: </label>
+      <br />
+      <input type="text" className="form-control" name="id"  readOnly value={gestorSeleccionado && gestorSeleccionado.id} />
+      <br />
+      <label>Nombre: </label>
+      <br />
+      <input type="text" className="form-control" name="nombre"  onChange={handleChange} value={gestorSeleccionado && gestorSeleccionado.nombre} />
+      <br />
+      <label>Lanzamiento: </label>
+      <br />
+      <input type="text" className="form-control" name="lanzamiento" onChange={handleChange} value={gestorSeleccionado && gestorSeleccionado.lanzamiento} />
+      <br />
+      <label>Desarrollador: </label>
+      <br />
+      <input type="text" className="form-control" name="desarrollador" onChange={handleChange} value={gestorSeleccionado && gestorSeleccionado.desarrollador} />
+      <br />
+    </div>
+  </ModalBody>
+  <ModalFooter>
+    <button className="btn btn-primary" onClick={()=>peticionPut()}>Editar</button>{" "}
+    <button className="btn btn-danger" onClick={()=>abrirCerrarModalEditar()}>Cancelar</button>
+  </ModalFooter>
+</Modal>
+
+<Modal isOpen={modalEliminar}>
+  <ModalBody>
+    ¿Estas seguro que deseas eliminar el gestor de base de datos {gestorSeleccionado && gestorSeleccionado.nombre}?
+  </ModalBody>
+        <ModalFooter>
+          <button className="btn btn-danger" onClick={()=>peticionDelete()}>Si</button>{" "}
+          <button className="btn btn-secondary" onClick={()=>abrirCerrarModalEliminar()}>No</button>
+        </ModalFooter>
+</Modal>
     </div>
   );
 }
